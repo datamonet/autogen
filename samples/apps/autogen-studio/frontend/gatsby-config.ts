46b727edc52ed7013602b4877a0963473ee11e1f
faye <faye_1225@163.com>
2024-07-31 10:41:11 +0800

    change:gatsby-config.ts

--- samples/apps/autogen-studio/frontend/gatsby-config.ts
+++ samples/apps/autogen-studio/frontend/gatsby-config.ts
@@ -33,7 +33,7 @@ const config: GatsbyConfig = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/favicon.png",
+        icon: "src/images/icon.png",
       },
     },
     "gatsby-plugin-mdx",
